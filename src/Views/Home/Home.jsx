69ca60e46a48bc6338c10f352314f30fb16a9bdc
FRickReich <frickreich@gmail.com>
2023-02-13 17:12:59 +0100

    Add link to dashboard route

--- src/Views/Home/Home.jsx
+++ src/Views/Home/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 export const Home = ({ ...props }) => {
 
     return (
+        <>
         <div>Hello World</div>
+        <div>
+            Link to <Link to="/dashboard">Dashboard</Link>.
+        </div>
+        </>
     )
 }
